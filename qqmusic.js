c77bb1172e01bdd4eaf76cebb64282c212b5c6fc
immrx113 <18969878257@163.com>
2026-03-09 17:37:40 +0800

    完成

--- qqmusic.js
+++ qqmusic.js
@@ -1,48 +1,91 @@
-let index=0;
+let recommend_index=0;
+let newsong_index=0;
 
-const slider=document.querySelector('.recommend-slider');
-const leftBtn=document.querySelector('.arrow-left');
-const rightBtn=document.querySelector('.arrow-right');
-const nulls=document.querySelectorAll('.content-recommend-bottom-null');
+const recommend_slider=document.querySelector('.recommend-slider');
+const recommend_leftBtn=document.querySelector('.arrow-left');
+const recommend_rightBtn=document.querySelector('.arrow-right');
+const recommend_nulls=document.querySelectorAll('.content-recommend-bottom-null');
+const newsong_slider=document.querySelector('.newsong-slider');
+const newsong_leftBtn=document.querySelector('.newsong-arrow-left');
+const newsong_rightBtn=document.querySelector('.newsong-arrow-right');
+const newsong_nulls=document.querySelectorAll('.null');
 
 const moveWidth=1200
-const maxIndex=4
+const recommend_maxIndex=4
+const newsong_maxIndex=5
 
 function updateNulls(){
-    nulls.forEach(function(dot){
+    recommend_nulls.forEach(function(dot){
         dot.classList.remove('null-active');
     })
-    nulls[index].classList.add('null-active');
+    recommend_nulls[recommend_index].classList.add('null-active');
+}
+function updateNewsongNulls(){
+    newsong_nulls.forEach(function(dot){
+        dot.classList.remove('null-active');
+    })
+    newsong_nulls[newsong_index].classList.add('null-active');
 }
 
-rightBtn.onclick=function(){
-    if(index<maxIndex){
-        index++;
-        slider.style.transform=`translateX(${-moveWidth*index}px)`;
+recommend_rightBtn.onclick=function(){
+    if(recommend_index<recommend_maxIndex){
+        recommend_index++;
+        recommend_slider.style.transform=`translateX(${-moveWidth*recommend_index}px)`;
     }
-    else if(index===maxIndex){
-        index=0;
-        slider.style.transform=`translateX(0px)`;
-    }
-    updateNulls();
-}
-leftBtn.onclick=function(){
-    if(index>0){
-
-        index--;
-        slider.style.transform=`translateX(${moveWidth*(-index)}px)`;
-    }
-    else if(index===0){
-        index=maxIndex;
-        slider.style.transform=`translateX(${-moveWidth*index}px)`;
+    else if(recommend_index===recommend_maxIndex){
+        recommend_index=0;
+        recommend_slider.style.transform=`translateX(0px)`;
     }
     updateNulls();
 }
 
-nulls.forEach(function(dot,i){
+newsong_rightBtn.onclick=function(){
+    if(newsong_index<newsong_maxIndex){
+        newsong_index++;
+        newsong_slider.style.transform=`translateX(${-moveWidth*newsong_index}px)`;
+    }
+    else if(newsong_index===newsong_maxIndex){
+        newsong_index=0;
+        newsong_slider.style.transform=`translateX(0px)`;
+    }
+    updateNewsongNulls();
+}
+
+recommend_leftBtn.onclick=function(){
+    if(recommend_index>0){
+
+        recommend_index--;
+        recommend_slider.style.transform=`translateX(${moveWidth*(-recommend_index)}px)`;
+    }
+    else if(recommend_index===0){
+        recommend_index=maxIndex;
+        recommend_slider.style.transform=`translateX(${-moveWidth*recommend_index}px)`;
+    }
+    updateNulls();
+}
+newsong_leftBtn.onclick=function(){
+    if(newsong_index>0){
+        newsong_index--;
+        newsong_slider.style.transform=`translateX(${moveWidth*(-newsong_index)}px)`;
+    }
+    else if(newsong_index===0){
+        newsong_index=newsong_maxIndex;
+        newsong_slider.style.transform=`translateX(${-moveWidth*newsong_index}px)`;
+    }
+    updateNewsongNulls();
+}
+
+recommend_nulls.forEach(function(dot,i){
     dot.onclick=function(){
-        index=i;
-        slider.style.transform=`translateX(${-moveWidth*index}px)`;
+        recommend_index=i;
+        recommend_slider.style.transform=`translateX(${-moveWidth*recommend_index}px)`;
         updateNulls();
+    }
+})
+newsong_nulls.forEach(function(dot,i){
+    dot.onclick=function(){
+        newsong_index=i;
+        newsong_slider.style.transform=`translateX(${-moveWidth*newsong_index}px)`;
+        updateNewsongNulls();
     }
 })
