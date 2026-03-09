9e4d64dd41e6dc3ed472f2e87616c762c2909c4d
immrx113 <18969878257@163.com>
2026-03-09 13:08:04 +0800

    完成歌单推荐部分轮播图
    完成歌单推荐，后面要修改代码，在完成新歌首发部分

--- qqmusic.js
+++ qqmusic.js
@@ -3,9 +3,17 @@ let index=0;
 const slider=document.querySelector('.recommend-slider');
 const leftBtn=document.querySelector('.arrow-left');
 const rightBtn=document.querySelector('.arrow-right');
+const nulls=document.querySelectorAll('.content-recommend-bottom-null');
 
 const moveWidth=1200
 const maxIndex=4
+
+function updateNulls(){
+    nulls.forEach(function(dot){
+        dot.classList.remove('null-active');
+    })
+    nulls[index].classList.add('null-active');
+}
 
 rightBtn.onclick=function(){
     if(index<maxIndex){
@@ -16,6 +24,7 @@ rightBtn.onclick=function(){
         index=0;
         slider.style.transform=`translateX(0px)`;
     }
+    updateNulls();
 }
 leftBtn.onclick=function(){
     if(index>0){
@@ -27,4 +36,13 @@ leftBtn.onclick=function(){
         index=maxIndex;
         slider.style.transform=`translateX(${-moveWidth*index}px)`;
     }
+    updateNulls();
 }
+
+nulls.forEach(function(dot,i){
+    dot.onclick=function(){
+        index=i;
+        slider.style.transform=`translateX(${-moveWidth*index}px)`;
+        updateNulls();
+    }
+})
